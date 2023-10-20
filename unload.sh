fd784744176e5bff3c6bb30db277a44a24b9769d
Mathieu ABATI <m.abati@trusted-objects.com>
2023-10-20 11:37:42 +0200

    Cleanup improvements

--- unload.sh
+++ unload.sh
@@ -2,6 +2,7 @@ if [ -z "${BB_TARGET_VAR_DOCKER_ROOTLESS}" ] || [[ "${BB_TARGET_VAR_DOCKER_ROOTL
 	SUDO="sudo"
 fi
 
+# Stop Docker daemon
 if [ -f "${DOCKER_PID_FILE}" ]; then
 	pid=$(cat ${DOCKER_PID_FILE})
 	ps -p ${pid} > /dev/null 2>&1
@@ -31,29 +32,36 @@ if [ -f "${DOCKER_PID_FILE}" ]; then
 	fi
 fi
 
-if [ -d "${DOCKER_EXEC_ROOT}" ]; then
-	${SUDO} chmod -R u+rwX "${DOCKER_EXEC_ROOT}" > /dev/null 2>&1
-	${SUDO} rm -rf "${DOCKER_EXEC_ROOT}"
-fi
-if [ -d "${DOCKER_DATA_ROOT}" ]; then
-	${SUDO} chmod -R u+rwX "${DOCKER_DATA_ROOT}" > /dev/null 2>&1
-fi
+# Umount Docker data root if needed
 if [ -z "${BB_TARGET_VAR_DOCKER_ROOTLESS}" ] || [[ "${BB_TARGET_VAR_DOCKER_ROOTLESS}" != "1" ]]; then
-	if [ -d "${DOCKER_CONFIG_DIR}" ]; then
-		sudo chown -R buildbox:buildbox "${DOCKER_CONFIG_DIR}"
-	fi
 	if [ -d "${DOCKER_DATA_ROOT}" ]; then
 		mountpoint -q "${DOCKER_DATA_ROOT}"
 		if [ $? -eq 0 ]; then
 			sudo umount "${DOCKER_DATA_ROOT}"
 		fi
-		sudo chown -R buildbox:buildbox "${DOCKER_DATA_ROOT}"
-	fi
-	if [ -d "${DOCKER_LOGS_DIR}" ]; then
-		sudo chown -R buildbox:buildbox "${DOCKER_LOGS_DIR}"
 	fi
 fi
 
+# Give permissions for buildbox user to manage all Docker created files
+if [ -d "${DOCKER_EXEC_ROOT}" ]; then
+	sudo setfacl -R -m u:buildbox:rwx ${DOCKER_EXEC_ROOT}
+fi
+if [ -d "${DOCKER_DATA_ROOT}" ]; then
+	sudo setfacl -R -m u:buildbox:rwx ${DOCKER_DATA_ROOT}
+fi
+if [ -d "${DOCKER_CONFIG_DIR}" ]; then
+	sudo setfacl -R -m u:buildbox:rwx ${DOCKER_CONFIG_DIR}
+fi
+if [ -d "${DOCKER_LOGS_DIR}" ]; then
+	sudo setfacl -R -m u:buildbox:rwx ${DOCKER_LOGS_DIR}
+fi
+
+# Cleanup
+if [ -d "${DOCKER_EXEC_ROOT}" ]; then
+	rm -rf "${DOCKER_EXEC_ROOT}"
+fi
+
+# Reset environment
 unset DOCKER_CONFIG_FILE
 unset DOCKER_CONFIG_DIR
 unset DOCKER_DATA_ROOT
