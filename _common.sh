7c1764392a29b41b37e2de1ee0f1e189bd4b1b90
Mathieu ABATI <m.abati@trusted-objects.com>
2024-04-29 17:48:16 +0200

    Improved lock handling

--- _common.sh
+++ _common.sh
@@ -1,6 +1,7 @@
 DOCKER_ENV_FILE=${BB_TARGET_BUILD_DIR}/docker.env
 DOCKER_ENV_RESET_FILE=${BB_TARGET_BUILD_DIR}/docker.env.reset
 DOCKER_ENV_LOCK=${BB_TARGET_BUILD_DIR}/docker.env.lock
+LOCK_OWNER=0
 
 function reset_docker_env {
 	if [ -f ${DOCKER_ENV_RESET_FILE} ]; then
@@ -11,9 +12,22 @@ function reset_docker_env {
 function lock_docker_env {
 	mkdir -p $(dirname ${DOCKER_ENV_LOCK})
 	mkdir ${DOCKER_ENV_LOCK} 2>/dev/null;
+	if [ $? -eq 0 ]; then
+		LOCK_OWNER=1
+		return 0
+	else
+		return 1
+	fi
 }
 
 function unlock_docker_env {
-	rmdir ${DOCKER_ENV_LOCK}
+	LOCK_OWNER=0
+	rmdir ${DOCKER_ENV_LOCK} > /dev/null 2>&1
 }
 
+function docker_tools_error_handler {
+	if [ ${LOCK_OWNER} -ne 0 ]; then
+		unlock_docker_env
+	fi
+}
+trap 'docker_tools_error_handler' ERR
