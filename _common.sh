628cdf2ebc53d63e13fc9d96d7800929f4a35b01
Mathieu ABATI <m.abati@trusted-objects.com>
2026-04-16 15:49:41 +0200

    Add license information

--- _common.sh
+++ _common.sh
@@ -1,3 +1,19 @@
+# Docker tools for BuildBox - common functions
+# Copyright (C) 2023-2026 Trusted Objects
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2, as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# <https://www.gnu.org/licenses/>.
+
 DOCKER_ENV_FILE=${BB_TARGET_BUILD_DIR}/docker.env
 DOCKER_ENV_RESET_FILE=${BB_TARGET_BUILD_DIR}/docker.env.reset
 DOCKER_ENV_LOCK=${BB_TARGET_BUILD_DIR}/docker.env.lock
